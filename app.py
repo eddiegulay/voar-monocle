461c87fdc21b693615ba5095d2e3d55c68695014
eddiegulay <edgargulay@outlook.com>
2024-07-11 03:02:49 +0300

    prep to push

---
+++ app.py
@@ -0,0 +1,107 @@
+import streamlit as st
+import os
+import pandas as pd
+
+from controls import *
+
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+# Function to create directory if it doesn't exist
+def create_directory(directory):
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+# Function to save uploaded file to specified directory
+def save_uploaded_file(uploaded_file, directory):
+    create_directory(directory)
+    with open(os.path.join(directory, uploaded_file.name), "wb") as f:
+        f.write(uploaded_file.getbuffer())
+    return os.path.join(directory, uploaded_file.name)
+
+def display_metrics(title, stats):
+    if title != "":
+        st.subheader(title)
+    cols = st.columns(len(stats))
+    for col, (metric, value) in zip(cols, stats.items()):
+        col.write(f"**{metric}:** \n{value}")
+
+def main():
+    st.title("Monocle 🧐")
+
+    # Define directories for uploaded files
+    crdb_directory = "uploaded_files/crdb"
+    lending_directory = "uploaded_files/lending"
+
+    # Check if files already exist
+    crdb_files = os.listdir(crdb_directory) if os.path.exists(crdb_directory) else []
+    lending_files = os.listdir(lending_directory) if os.path.exists(lending_directory) else []
+
+    # Initialize variables
+    crdb_file_path = None
+    lending_file_path = None
+
+    # Sidebar with file upload widgets
+    st.sidebar.title("Upload Files")
+    crdb_file = st.sidebar.file_uploader("Upload CRDB Bank Statement CSV", type="csv")
+    lending_file = st.sidebar.file_uploader("Upload Lending Company Payment Document CSV", type="csv")
+
+    if crdb_file:
+        crdb_file_path = save_uploaded_file(crdb_file, crdb_directory)
+    elif crdb_files:
+        crdb_file_path = os.path.join(crdb_directory, crdb_files[0])
+
+    if lending_file:
+        lending_file_path = save_uploaded_file(lending_file, lending_directory)
+    elif lending_files:
+        lending_file_path = os.path.join(lending_directory, lending_files[0])
+
+    # If files are available, process them
+    if crdb_file_path and lending_file_path:
+        bank_statement = pd.read_csv(crdb_file_path)
+        lending_statement = pd.read_csv(lending_file_path)
+
+        # Normalize data
+        bank_statement = normalize_bank_data(bank_statement)
+        lending_statement = normalize_lender_data(lending_statement)
+
+        # Fill NaN in ismatched with 'Not Checked'
+        try:
+            lending_statement['ismatched'] = lending_statement['ismatched'].fillna('Not Checked')
+            lending_statement['POP'] = lending_statement['POP'].fillna('No PoP Provided')
+        except KeyError:
+            logger.info("No ismatched or POP columns in lending statement")
+
+        # Get File stats
+        bank_stats, bank_credit_debit = get_bank_stats(bank_statement)
+        lender_stats, credit_debit = get_lender_stats(lending_statement)
+
+        # Display Bank Statement Stats
+        display_metrics("Bank Statement Stats", bank_stats)
+        display_metrics("", bank_credit_debit)
+
+        # Display Lending Company Payment Document Stats
+        display_metrics("Airtable Document Stats", lender_stats)
+        display_metrics("", credit_debit)
+
+        # check missing from lender
+        missing_from_lender = check_missing_from_lender(bank_statement, lending_statement)
+        if len(missing_from_lender) > 0:
+            # Display missing from lender
+            st.title("Records missing from Airtable:")
+            st.write(missing_from_lender.drop(columns=['normalized_description', 'Book Balance']))
+
+            # total missing amount missing from lender
+            total_missing_amount = missing_from_lender['Debit'].sum()
+            total_missing_amount = locale.currency(total_missing_amount, grouping=True)
+            st.write(f"Total Missing Amount: {total_missing_amount}")
+        else:
+            st.info("No records missing from Airtable")
+
+    else:
+        st.info("Please upload both CSV files to start reconciliation.")
+
+if __name__ == "__main__":
+    main()
---
+++ controls.py
@@ -0,0 +1,152 @@
+import pandas as pd
+import string
+import locale
+
+locale.setlocale(locale.LC_ALL, 'sw_TZ.UTF-8')
+
+
+def normalize_lender_data(lender_df):
+    """
+    Normalize the lender data in a DataFrame.
+
+    This function converts the 'created_at' column to a datetime format (day.month.year),
+    converts the 'credit' and 'debit' columns to numeric types, coercing any errors,
+    and adds a 'normalized_description' column which is the 'description' column in
+    lower case, without punctuation marks and spaces.
+
+    Parameters:
+    - lender_df (pd.DataFrame): The DataFrame containing lender data. It must have
+    'created_at', 'credit', 'debit', and 'description' columns.
+
+    Returns:
+    - pd.DataFrame: The normalized DataFrame with the specified columns converted.
+    """
+    # Normalize 'created_at' date format to dd.mm.yyyy
+    lender_df['created_at'] = pd.to_datetime(lender_df['created_at']).dt.strftime('%d.%m.%Y')
+
+    # Convert 'credit' and 'debit' columns to numeric
+    lender_df['credit'] = pd.to_numeric(lender_df['credit'], errors='coerce')
+    lender_df['debit'] = pd.to_numeric(lender_df['debit'], errors='coerce')
+
+    # Normalize 'description' column
+    lender_df['normalized_description'] = lender_df['description'].apply(lambda x: ''.join(e for e in x.lower() if e.isalnum()))
+
+    return lender_df
+
+
+
+def normalize_bank_data(bank_df):
+    """
+    Normalize the bank data in a DataFrame.
+
+    This function converts the 'Posting Date' and 'Value Date' columns to a datetime format (day.month.year),
+    converts the 'Debit' and 'Credit' columns to numeric types, coercing any errors,
+    and adds a 'normalized_description' column which is the 'Details' column in
+    lower case, without punctuation marks and spaces.
+
+    Parameters:
+    - bank_df (pd.DataFrame): The DataFrame containing bank data. It must have
+    'Posting Date', 'Value Date', 'Debit', 'Credit', and 'Details' columns.
+
+    Returns:
+    - pd.DataFrame: The normalized DataFrame with the specified columns converted.
+    """
+
+    def parse_date(date_str):
+        # Attempt to parse the date string with the expected format
+        try:
+            return pd.to_datetime(date_str, format='%d.%m.%Y %H:%M:%S').strftime('%d.%m.%Y')
+        except ValueError:
+            # If parsing fails, try to parse it with a more flexible approach
+            try:
+                return pd.to_datetime(date_str, dayfirst=True).strftime('%d.%m.%Y')
+            except ValueError:
+                # If all parsing attempts fail, return the original string
+                return date_str
+
+    # Normalize 'Posting Date' and 'Value Date' date format to dd.mm.yyyy
+    bank_df['Posting Date'] = bank_df['Posting Date'].apply(parse_date)
+    bank_df['Value Date'] = bank_df['Value Date'].apply(parse_date)
+
+
+    # Convert 'Debit' and 'Credit' columns to numeric
+    bank_df['Debit'] = bank_df['Debit'].str.replace(',', '').astype(float)
+    bank_df['Credit'] = bank_df['Credit'].str.replace(',', '').astype(float)
+
+    # Normalize 'Details' column
+    bank_df['normalized_description'] = bank_df['Details'].apply(lambda x: ''.join(e for e in x.lower() if e.isalnum()))
+
+    return bank_df[bank_df['normalized_description'].str.contains('ramani') & (bank_df['Credit'] == 0)]
+
+
+
+def get_lender_stats(lender_statement):
+    """
+    Get Dataset Stats
+    input: lender_statement (pd.DataFrame)
+    output: dict
+    """
+    results = {}
+    credit_debit = {}
+
+    # Total number of records
+    results['records'] = len(lender_statement)
+    results['matched'] = len(lender_statement[lender_statement['ismatched'] == 'checked'])
+    results['unmatched'] = len(lender_statement[lender_statement['ismatched'] == 'Not Checked'])
+    results['PoP'] = len(lender_statement[lender_statement['POP'] != 'No PoP Provided'])
+    results['no_PoP'] = len(lender_statement[lender_statement['POP'] == 'No PoP Provided'])
+    total_credit = lender_statement[lender_statement['credit'].notnull()]['credit'].sum()
+    total_debit = lender_statement[lender_statement['debit'].notnull()]['debit'].sum()
+
+    # Format as currency
+    credit_debit['credit_amount'] = locale.currency(total_credit, grouping=True)
+    credit_debit['debit_amount'] = locale.currency(total_debit, grouping=True)
+
+    return results, credit_debit
+
+def get_bank_stats(bank_statement):
+    """
+    Get Dataset Stats
+    input: bank_statement (pd.DataFrame)
+    output: dict
+    """
+    results = {}
+    credit_debit = {}
+
+    # Total number of records
+    results['records'] = len(bank_statement)
+    total_credit = bank_statement['Credit'].sum()
+    total_debit = bank_statement['Debit'].sum()
+
+    # Min - Max Value Date
+    results['From'] = bank_statement['Value Date'].min()
+    results['To'] = bank_statement['Value Date'].max()
+
+    # Format as currency
+    results['records'] = len(bank_statement)
+    credit_debit['credit'] = locale.currency(total_credit, grouping=True)
+    credit_debit['debit'] = locale.currency(total_debit, grouping=True)
+
+    return results, credit_debit
+
+
+
+def check_missing_from_lender(bank_statement, lender_statement):
+    """
+    Check if there are any records in the bank statement that are missing from the lender statement.
+
+    Parameters:
+    - bank_statement (pd.DataFrame): The bank statement DataFrame.
+    - lender_statement (pd.DataFrame): The lender statement DataFrame.
+
+    Returns:
+    - pd.DataFrame: A DataFrame containing the records in the bank statement that are missing from the lender statement.
+    """
+    # Get the normalized descriptions from both DataFrames
+    bank_descriptions = bank_statement['normalized_description']
+    lender_descriptions = lender_statement['normalized_description']
+
+    # Find the records in the bank statement that are missing from the lender statement
+    missing_records = bank_statement[~bank_descriptions.isin(lender_descriptions)]
+
+    return missing_records
